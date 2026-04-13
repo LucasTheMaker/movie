8a30e5cee48f455edc4058fd07464307220c20b3
LucasTheMaker <msntbn6n8p@privaterelay.appleid.com>
2026-04-12 17:45:45 -0700

    Add Kanye West album data to kanye_data.js

---
+++ kanye_data.js
@@ -0,0 +1,67 @@
+const KANYE_DATABASE = {
+    "THE_COLLEGE_DROPOUT [E]": {
+        "year": 2004,
+        "distributor": "Roc-A-Fella / Def Jam",
+        "tracks": ["Intro", "We Don't Care [E]", "Graduation Day [E]", "All Falls Down [E]", "I'll Fly Away", "Spaceship [E]", "Jesus Walks", "Never Let Me Down [E]", "Get Em High [E]", "Workout Plan", "The New Workout Plan [E]", "Slow Jamz [E]", "Breathe In Breathe Out [E]", "School Spirit Skit 1", "School Spirit", "School Spirit Skit 2", "Lil Jimmy Skit", "Two Words [E]", "Through The Wire", "Family Business", "Last Call [E]"]
+    },
+    "LATE_REGISTRATION [E]": {
+        "year": 2005,
+        "distributor": "Roc-A-Fella / Def Jam",
+        "tracks": ["Wake Up Mr. West", "Heard 'Em Say", "Touch the Sky [E]", "Gold Digger [E]", "Skit #1", "Drive Slow [E]", "Crack Music [E]", "Roses", "Bring Me Down [E]", "Skit #2", "Addiction [E]", "Skit #3", "Diamonds From Sierra Leone (Remix) [E]", "We Major [E]", "Skit #4", "Hey Mama", "Celebration [E]", "Skit #5", "Gone [E]", "Diamonds From Sierra Leone [E]"]
+    },
+    "GRADUATION [E]": {
+        "year": 2007,
+        "distributor": "Roc-A-Fella / Def Jam",
+        "tracks": ["Good Morning", "Champion", "Stronger [E]", "I Wonder [E]", "Good Life [E]", "Can't Tell Me Nothing [E]", "Barry Bonds [E]", "Flashing Lights", "Everything I Am [E]", "The Glory [E]", "Homecoming", "Big Brother [E]", "Good Night"]
+    },
+    "808S_AND_HEARTBREAK": {
+        "year": 2008,
+        "distributor": "Roc-A-Fella / Def Jam",
+        "tracks": ["Say You Will", "Welcome to Heartbreak", "Heartless", "Amazing", "Love Lockdown", "Paranoid", "RoboCop", "Street Lights", "Bad News", "See You in My Nightmares", "Coldest Winter", "Pinocchio Story"]
+    },
+    "MBDTF [E]": {
+        "year": 2010,
+        "distributor": "Roc-A-Fella / Def Jam",
+        "tracks": ["Dark Fantasy [E]", "Gorgeous [E]", "Power [E]", "All of the Lights (Interlude)", "All of the Lights [E]", "Monster [E]", "So Appalled [E]", "Devil in a New Dress [E]", "Runaway [E]", "Hell of a Life [E]", "Blame Game [E]", "Lost in the World [E]", "Who Will Survive in America [E]"]
+    },
+    "WATCH_THE_THRONE [E]": {
+        "year": 2011,
+        "distributor": "Roc-A-Fella / Def Jam",
+        "tracks": ["No Church in the Wild [E]", "Lift Off", "Ni**as in Paris [E]", "Otis [E]", "Gotta Have It [E]", "New Day [E]", "That's My B****", "Welcome to the Jungle [E]", "Who Gon Stop Me [E]", "Murder to Excellence [E]", "Made in America", "Why I Love You [E]", "Illest Motherf***er Alive [E]", "H•A•M [E]", "Primetime [E]", "The Joy [E]"]
+    },
+    "YEEZUS [E]": {
+        "year": 2013,
+        "distributor": "Def Jam",
+        "tracks": ["On Sight [E]", "Black Skinhead [E]", "I Am a God [E]", "New Slaves [E]", "Hold My Liquor [E]", "I'm In It [E]", "Blood on the Leaves [E]", "Guilt Trip [E]", "Send It Up [E]", "Bound 2 [E]"]
+    },
+    "THE_LIFE_OF_PABLO [E]": {
+        "year": 2016,
+        "distributor": "G.O.O.D. Music / Def Jam",
+        "tracks": ["Ultralight Beam", "Father Stretch My Hands Pt. 1 [E]", "Pt. 2 [E]", "Famous [E]", "Feedback [E]", "Low Lights", "Highlights [E]", "Freestyle 4 [E]", "I Love Kanye [E]", "Waves [E]", "FML [E]", "Real Friends [E]", "Wolves", "Frank's Track", "Siiiiiiiiilver Surffffeeeeer Intermission", "30 Hours [E]", "No More Parties in LA [E]", "Facts (Charlie Heat Version) [E]", "Fade [E]", "Saint Pablo [E]"]
+    },
+    "YE [E]": {
+        "year": 2018,
+        "distributor": "G.O.O.D. Music / Def Jam",
+        "tracks": ["I Thought About Killing You [E]", "Yikes [E]", "All Mine [E]", "Wouldn't Leave [E]", "No Mistakes [E]", "Ghost Town [E]", "Violent Crimes"]
+    },
+    "DONDA_DELUXE": {
+        "year": 2021,
+        "distributor": "G.O.O.D. Music / Def Jam",
+        "tracks": ["Donda Chant", "Hurricane", "Moon", "Life Of The Party", "Off The Grid", "Jail", "Praise God", "Come to Life", "Believe What I Say", "No Child Left Behind", "Up From The Ashes", "Remote Control pt 2", "God Breathed", "Lord I Need You", "24", "Junya", "Never Abandon Your Family", "Donda", "Keep My Spirit Alive", "Jesus Lord", "New Again", "Tell The Vision", "Pure Souls", "Heaven and Hell", "Ok Ok", "Jonah", "Insta", "Jail pt 2", "Ok Ok pt 2", "Junya pt 2", "Jesus Lord pt 2", "Keep My Spirit Alive pt 2"]
+    },
+    "VULTURES_1 [E]": {
+        "year": 2024,
+        "distributor": "YZY / Label Engine",
+        "tracks": ["STARS [E]", "KEYS TO MY LIFE [E]", "PAID [E]", "TALKING", "BACK TO ME [E]", "HOODRAT [E]", "DO IT [E]", "PAPERWORK [E]", "BURN [E]", "FUK SUMN [E]", "VULTURES [E]", "CARNIVAL [E]", "BEG FORGIVENESS [E]", "GOOD (DON'T DIE)", "PROBLEMATIC [E]", "KING [E]"]
+    },
+    "VULTURES_2 [E]": {
+        "year": 2024,
+        "distributor": "YZY / Label Engine",
+        "tracks": ["SLIDE [E]", "TIME MOVING SLOW [E]", "FIELD TRIP [E]", "FRIED [E]", "ISABELLA", "PROMOTION [E]", "HUSBAND [E]", "LIFESTYLE [E]", "FOREVER [E]", "BOMB", "RIVER [E]", "530 [E]", "DEAD [E]", "FOREVER ROLLING [E]", "SKY CITY [E]", "MY SOUL [E]", "TAKE OFF YOUR DRESS [E]", "BELIEVER [E]", "DRUNK [E]", "GUN TO MY HEAD [E]", "CAN U BE [E]"]
+    },
+    "BULLY [E]": {
+        "year": 2026,
+        "distributor": "YZY / Gamma",
+        "tracks": ["KING [E]", "THIS A MUST [E]", "FATHER [E]", "ALL THE LOVE [E]", "PUNCH DRUNK [E]", "WHATEVER WORKS [E]", "MAMA'S FAVORITE [E]", "SISTERS AND BROTHERS", "BULLY [E]", "HIGHS AND LOWS [E]", "I CAN'T WAIT [E]", "WHITE LINES [E]", "CIRCLES [E]", "PREACHER MAN [E]", "BEAUTY AND THE BEAST [E]", "DAMN [E]", "LAST BREATH [E]", "THIS ONE HERE [E]"]
+    }
+};
